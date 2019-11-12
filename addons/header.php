2cf5142aabeb901abbe20809caec26370e1ad650
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-11-12 16:53:01 +0100

    Initial commit

---
+++ addons/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/header.css">
+    <title>wiki-how</title>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rancho&effect=fire-animation" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<!------- dit is de header --->
+<header>
+    <div class="nav_holder">
+        <nav>
+            <ul class="unordered_list">
+                <li><a href="../index.php">home</a></li>
+                <li><a href="#">test 2</a></li>
+                <li><a href="#">test 3</a></li>
+                <li><a href="#">test 4</a></li>
+            </ul>
+        </nav>
+        <div class="tooth1"></div>
+        <div class="tooth2"></div>
+    </div>
+
+    <div class="explosie">
+        <div class="oranje">
+            <div class="geel">
+                <div class="wit"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="broken_part">
+        <div class="tooth3"></div>
+        <div class="tooth4"></div>
+        <a class="eerste_helft font-effect-fire-animation" href="#">pag</a>
+        <a class="tweede_helft font-effect-fire-animation" href="#">ina 5</a>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include_once "addons/header.php";?>
+<main>
+    <div class="shadow_item_1"></div>
+    <div class="item_1"></div>
+    <div id="dna_holder" class="dna1">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna2">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna3">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna4">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna5">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna6">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna7">
+        <img src="image/dna_paars.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna8">
+        <img src="image/dna_paars.png" alt="">
+    </div>
+    <div id="dna_holder" class="dna9">
+        <img src="image/dna_paars.png" alt="">
+    </div>
+
+
+
+
+
+
+</main>
