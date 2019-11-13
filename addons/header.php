d88aff368829a138110c45855df7553a539b2a2b
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-11-13 16:53:19 +0100

    dit is de tweede commit die is uitgevoerd op deze file

--- addons/header.php
+++ addons/header.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rancho&effect=fire-animation" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <!------- dit is de header --->
--- index.php
+++ index.php
@@ -1,7 +1,31 @@
+<script src="https://kit.fontawesome.com/897dbd24c8.js" crossorigin="anonymous"></script>
 <?php include_once "addons/header.php";?>
 <main>
     <div class="shadow_item_1"></div>
-    <div class="item_1"></div>
+    <div class="item_1">
+        <!------------ next knop -------------->
+            <button type="submit" name="next" id="next">
+            <p>Next <i class="fas fa-caret-right"></i></p>
+            </button>
+        <!------------ einde next knop -------------->
+
+
+
+
+        <!----------- logo links boven ---------------------->
+        <div class="styling_item_1"></div>
+        <div class="styling_item_2">
+            <h1>K</h1>
+            <p>人</p>
+            <p>大</p>
+            <img src="image/plaatje1.png" alt="">
+        </div>
+        <!----------- einde logo links boven ---------------------->
+        <div class="styling_item_3"></div>
+    </div>
+
+
+    <!--------- dna spinners -------------->
     <div id="dna_holder" class="dna1">
         <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
     </div>
@@ -29,6 +53,7 @@
     <div id="dna_holder" class="dna9">
         <img src="image/dna_paars.png" alt="">
     </div>
+    <!--------------- einde dna spinners --------------->
 
 
 
