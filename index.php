03f433e6165f9fbc98ce7c3b0ce99d002d14a615
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-11-14 16:46:58 +0100

    dit is de tweede commit die is uitgevoerd op deze file

--- index.php
+++ index.php
@@ -1,48 +1,67 @@
 <script src="https://kit.fontawesome.com/897dbd24c8.js" crossorigin="anonymous"></script>
-<?php include_once "addons/header.php";?>
+<?php include_once "addons/header.php"; ?>
 <main>
-    <div class="shadow_item_1"></div>
-    <div class="item_1">
-        <!------------ next knop -------------->
+
+    <div id="q0">
+        <div class="shadow_item_1"></div>
+        <div class="item_1">
+            <!------------ next knop -------------->
             <button type="submit" name="next" id="next">
-            <p>Next <i class="fas fa-caret-right"></i></p>
+                <p>Next <i class="fas fa-caret-right"></i></p>
             </button>
-        <!------------ einde next knop -------------->
+            <!------------ einde next knop -------------->
 
-
-
-
-        <!----------- logo links boven ---------------------->
-        <div class="styling_item_1"></div>
-        <div class="styling_item_2">
-            <h1>K</h1>
-            <p>人</p>
-            <p>大</p>
-            <img src="image/plaatje1.png" alt="">
+            <!----------- logo links boven ---------------------->
+            <div class="styling_item_1"></div>
+            <div class="styling_item_2">
+                <h1>K</h1>
+                <p>人</p>
+                <p>大</p>
+                <img src="image/plaatje1.png" alt="">
+            </div>
+            <!----------- einde logo links boven ---------------------->
+            <div class="styling_item_3"></div>
         </div>
-        <!----------- einde logo links boven ---------------------->
-        <div class="styling_item_3"></div>
     </div>
+
+    <div id="q1">
+        <div class="item_2">
+        <div class="shadow_item_2">
+
+        </div>
+        </div>
+    </div>
+
+
+
+
+
 
 
     <!--------- dna spinners -------------->
     <div id="dna_holder" class="dna1">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna2">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna3">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna4">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna5">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna6">
-        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png" alt="">
+        <img src="image/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d759551760.1028175115573215613485.png"
+             alt="">
     </div>
     <div id="dna_holder" class="dna7">
         <img src="image/dna_paars.png" alt="">
@@ -54,10 +73,6 @@
         <img src="image/dna_paars.png" alt="">
     </div>
     <!--------------- einde dna spinners --------------->
-
-
-
-
 
 
 </main>
