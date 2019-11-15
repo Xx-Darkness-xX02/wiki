7cd8be9a7aaea8341f0b43481afb4c4c67174a5b
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-11-15 16:47:29 +0100

    dit is de tweede commit die is uitgevoerd op deze file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <script src="https://kit.fontawesome.com/897dbd24c8.js" crossorigin="anonymous"></script>
 <?php include_once "addons/header.php"; ?>
 <main>
-
+    <!------------------------------ eerste div ----------------------------->
     <div id="q0">
         <div class="shadow_item_1"></div>
         <div class="item_1">
@@ -23,19 +23,45 @@
             <div class="styling_item_3"></div>
         </div>
     </div>
+    <!--------------------------- einde eerste div -------------------------------------------->
 
+
+    <!----------------- tweede div -------------->
     <div id="q1">
-        <div class="item_2">
         <div class="shadow_item_2">
-
+            <div class="shadow_styling_item_1"></div>
+            <div class="shadow_styling_item_2"></div>
         </div>
+        <div class="item_2">
+            <div id="infinity"></div>
+            <div class="styling_item_4"></div>
+            <div class="styling_item_5"></div>
+
+            <!-- logo links boven --->
+            <div class="styling_item_6">
+                <h1>A</h1>
+                <p>柜</p>
+                <p>最</p>
+                <img src="image/plaatje1.png" alt="">
+            </div>
+            <!-- einde logo links boven --->
+
+            <button type="submit" name="next" id="next">
+                <p>Next <i class="fas fa-caret-right"></i></p>
+            </button>
         </div>
     </div>
+    <!----------------- einde tweede div ---------------->
 
 
-
-
-
+    <!--------------- derde div ----------------->
+    <div id="q2">
+        <div class="shadow_item_3"></div>
+        <div class="item_3">
+            <div id="yin_yang"></div>
+        </div>
+    </div>
+    <!--------------- einde derde div ---------------->
 
 
     <!--------- dna spinners -------------->
